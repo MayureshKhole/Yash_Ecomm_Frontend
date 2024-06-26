b6c528bfd8a94437b4718cd2e4a3d6237c1e1652
yashanbhore <anbhorey2003@gmail.com>
2024-06-26 16:51:54 +0530

    Home page UI Updated

--- src/components/BannerProduct.js
+++ src/components/BannerProduct.js
@@ -66,9 +66,9 @@ const BannerProduct = () => {
         <div className='h-56 md:h-72 w-full bg-slate-200 relative'>
 
                 <div className='absolute z-10 h-full w-full md:flex items-center hidden '>
-                    <div className=' flex justify-between w-full text-2xl'>
-                        <button onClick={preveImage} className='bg-white shadow-md rounded-full p-1'><FaAngleLeft/></button>
-                        <button onClick={nextImage} className='bg-white shadow-md rounded-full p-1'><FaAngleRight/></button> 
+                    <div className=' flex justify-between w-full h-48 text-2xl'>
+                        <button onClick={preveImage} className='bg-white shadow-md  p-1'><FaAngleLeft/></button>
+                        <button onClick={nextImage} className='bg-white shadow-md  p-1'><FaAngleRight/></button> 
                     </div>
                 </div>
 
