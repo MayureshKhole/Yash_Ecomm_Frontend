7da6c550bfa163ec3c1d887ebc789cd35c8358ad
yashanbhore <anbhorey2003@gmail.com>
2024-07-02 23:38:36 +0530

    Home page UI changed according to Product  - 4

--- src/helpers/productCategory.js
+++ src/helpers/productCategory.js
@@ -3,7 +3,7 @@ const productCategory = [
     { id : 2, label : "Ceiling", value : "ceiling"},
     { id : 3, label : "Floor", value : "floor"},
     { id : 4, label : "Wall", value : "wall"},
-    { id : 5, label : "Bathroom", value : "bathroom"}
+    { id : 5, label : "Table", value : "table"}
 ]
 
 
