bbffd7f1546f791e314216a1e43015d713fc208d
yashanbhore <anbhorey2003@gmail.com>
2024-07-02 23:02:18 +0530

    Home page UI changed according to Product  - 2

--- src/components/VerticalCardProduct.js
+++ src/components/VerticalCardProduct.js
@@ -43,80 +43,78 @@ const VerticalCardProduct = ({ category, heading }) => {
 
   return (
     <div className="container mx-auto px-4 my-6 relative">
-        <h2 className="text-pretty text-5xl from-neutral-950 py-4">{heading}</h2>
+      <h2 className="text-pretty text-5xl from-neutral-950 py-4">{heading}</h2>
 
-        <div
-          className="flex items-center gap-4 md:gap-6 overflow-x-scroll scrollbar-none transition-all"
-          ref={scrollElement}
+      <div
+        className="flex items-center gap-4 md:gap-6 overflow-x-scroll scrollbar-none transition-all"
+        ref={scrollElement}
+      >
+        <button
+          className="bg-white shadow-md rounded-full p-1 absolute left-4 text-lg hidden md:block"
+          onClick={scrollLeft}
         >
-          <button
-            className="bg-white shadow-md rounded-full p-1 absolute left-4 text-lg hidden md:block"
-            onClick={scrollLeft}
-          >
-            <FaAngleLeft />
-          </button>
-          <button
-            className="bg-white shadow-md rounded-full p-1 absolute right-4 text-lg hidden md:block"
-            onClick={scrollRight}
-          >
-            <FaAngleRight />
-          </button>
+          <FaAngleLeft />
+        </button>
+        <button
+          className="bg-white shadow-md rounded-full p-1 absolute right-4 text-lg hidden md:block"
+          onClick={scrollRight}
+        >
+          <FaAngleRight />
+        </button>
 
-          {loading
-            ? loadingList.map((product, index) => {
-                return (
-                  <div className="w-full min-w-[280px]  md:min-w-[320px] max-w-[280px] md:max-w-[320px]  bg-white rounded-sm shadow ">
-                    <div className="bg-slate-200 h-48 p-4 min-w-[280px] md:min-w-[145px] flex justify-center items-center animate-pulse"></div>
-                    <div className="p-4 grid gap-3">
-                      <h2 className="font-medium text-base md:text-lg text-ellipsis line-clamp-1 text-black p-1 py-2 animate-pulse rounded-full bg-slate-200"></h2>
-                      <p className="capitalize text-slate-500 p-1 animate-pulse rounded-full bg-slate-200  py-2"></p>
-                      <div className="flex gap-3">
-                        <p className="bg-[#ffc470] font-medium p-1 animate-pulse rounded-full bg-slate-200 w-full  py-2"></p>
-                        <p className="text-slate-500 line-through p-1 animate-pulse rounded-full bg-slate-200 w-full  py-2"></p>
-                      </div>
-                      <button className="text-sm  text-white px-3  rounded-full bg-slate-200  py-2 animate-pulse"></button>
+        {loading
+          ? loadingList.map((product, index) => {
+              return (
+                <div className="w-full min-w-[280px]  md:min-w-[320px] max-w-[280px] md:max-w-[320px]  bg-white rounded-sm shadow ">
+                  <div className="bg-slate-200 h-48 p-4 min-w-[280px] md:min-w-[145px] flex justify-center items-center animate-pulse"></div>
+                  <div className="p-4 grid gap-3">
+                    <h2 className="font-medium text-base md:text-lg text-ellipsis line-clamp-1 text-black p-1 py-2 animate-pulse rounded-full bg-slate-200"></h2>
+                    <p className="capitalize text-slate-500 p-1 animate-pulse rounded-full bg-slate-200  py-2"></p>
+                    <div className="flex gap-3">
+                      <p className="bg-[#ffc470] font-medium p-1 animate-pulse rounded-full bg-slate-200 w-full  py-2"></p>
+                      <p className="text-slate-500 line-through p-1 animate-pulse rounded-full bg-slate-200 w-full  py-2"></p>
                     </div>
+                    <button className="text-sm  text-white px-3  rounded-full bg-slate-200  py-2 animate-pulse"></button>
                   </div>
-                );
-              })
-            : data.map((product, index) => {
-                return (
-                  <Link
-                    to={"product/" + product?._id}
-                    className="w-full min-w-[280px]  md:min-w-[320px] max-w-[280px] md:max-w-[320px]  bg-white rounded-sm shadow "
-                  >
-                    <div className="bg-slate-200 h-48 p-4 min-w-[280px] md:min-w-[145px] flex justify-center items-center">
-                      <img
-                        src={product.productImage[0]}
-                        className="object-scale-down h-full hover:scale-110 transition-all mix-blend-multiply"
-                      />
-                    </div>
-                    <div className="p-4 grid gap-3">
-                      <h2 className="font-medium text-base md:text-lg text-ellipsis line-clamp-1 text-black">
-                        {product?.productName}
-                      </h2>
-                      <p className="capitalize text-slate-500">
-                        {product?.category}
+                </div>
+              );
+            })
+          : data.map((product, index) => {
+              return (
+                <Link
+                  to={"product/" + product?._id}
+                  className="w-full min-w-[280px]  md:min-w-[320px] max-w-[280px] md:max-w-[320px]  bg-white rounded-sm shadow "
+                >
+                  <img
+                    src={product.productImage[0]}
+                    className="object-scale-down h-full zoom-on-hover transition-all mix-blend-multiply"
+                  />
+                  <div className="p-4 grid gap-3">
+                    <h2 className="font-medium text-base md:text-lg text-ellipsis line-clamp-1 text-black">
+                      {product?.productName}
+                    </h2>
+                    <p className="capitalize text-slate-500">
+                      {product?.category}
+                    </p>
+                    <div className="flex gap-3">
+                      <p className="bg-[#ffc470] font-medium">
+                        {displayINRCurrency(product?.sellingPrice)}
                       </p>
-                      <div className="flex gap-3">
-                        <p className="bg-[#ffc470] font-medium">
-                          {displayINRCurrency(product?.sellingPrice)}
-                        </p>
-                        <p className="text-slate-500 line-through">
-                          {displayINRCurrency(product?.price)}
-                        </p>
-                      </div>
-                      <button
-                        className="text-sm bg-[#ffc470] hover:bg-[#ffc470] text-black px-3 py-0.5 rounded-full"
-                        onClick={(e) => handleAddToCart(e, product?._id)}
-                      >
-                        Add to Cart
-                      </button>
+                      <p className="text-slate-500 line-through">
+                        {displayINRCurrency(product?.price)}
+                      </p>
                     </div>
-                  </Link>
-                );
-              })}
-        </div>
+                    <button
+                      className="text-sm bg-[#ffc470] hover:bg-[#ffc470] text-black px-3 py-0.5 rounded-full"
+                      onClick={(e) => handleAddToCart(e, product?._id)}
+                    >
+                      Add to Cart
+                    </button>
+                  </div>
+                </Link>
+              );
+            })}
+      </div>
     </div>
   );
 };
